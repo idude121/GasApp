f6de2418940ca76ed0252a5ae46c255eed4fd1ec
Ian McManus <mcmanus.ian.g@gmail.com>
2023-04-30 21:52:20 -0500

    project part 2

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, SafeAreaView, Alert, FlatList } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, SafeAreaView, Alert, FlatList, Platform } from 'react-native';
 import ImageViewer from './components/ImageViewer';
 import * as SplashScreen from 'expo-splash-screen';
-import { useState, useRef, Component } from 'react';
+import { useState, useRef, Component, useEffect } from 'react';
 import Button from './components/Button';
 import * as ImagePicker from 'expo-image-picker'
 import * as MediaLibrary from 'expo-media-library';
@@ -12,9 +12,86 @@ import { DrawerItem, createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { ScrollView } from 'react-native-gesture-handler';
 import * as WebBrowser from 'expo-web-browser';
+import * as SQLite from 'expo-sqlite';
 
+
+const Drawer = createDrawerNavigator();
+ 
+function openDatabase() {
+  if (Platform.OS === 'web') {
+    return {
+      transaction: () => {
+        return {
+          executeSql: () => {},
+        };
+      },
+    };
+  }
+  const db = SQLite.openDatabase('mpg.db');
+  return db;
+}
+
+const db = openDatabase();
 
 export default function App()  {
+  const [forceUpdate, forceUpdateId] = useForceUpdate();
+  const [mpgEntry, setMpgEntry] = useState([]);
+
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'SELECT * FROM mpgEntries order by id desc',
+        null,
+        (_, { rows: { _array } }) => setMpgEntry(_array)
+      );
+    });
+  }, []);
+
+
+  function MpgEntries({ props })  {
+    const mpgEntry = props.mpgEntry;
+    useEffect(() => {
+      db.transaction(tx => {
+        tx.executeSql(
+          'SELECT * FROM mpgEntries order by id desc',
+          null,
+          (_, { rows: { _array } }) => setMpgEntry(_array)
+        );
+      });
+    }, []);
+
+    function renderRow({ item }) {
+      if (!item) {
+        return (
+          <View style={styles.row}>
+            <View style={styles.info}>
+              <Text style={styles.title}>No entries yet</Text>
+            </View>
+          </View>
+        );
+      } else {
+        return (
+          <View style={styles.row}>
+            <View style={styles.info}>
+              <Text style={styles.title}>Odometer miles: {item.odoMiles}</Text>
+              <Text style={styles.title}>Miles since last fill: {item.milesSinceLast}</Text>
+              <Text style={styles.title}>MPG: {item.mpg}</Text>
+              <Text style={styles.title}>Cost of fill: {item.costOfFill}</Text>
+              <Text style={styles.title}>Date: {item.date}</Text>
+            </View>
+          </View>
+        );
+      }
+    }
+    
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList data={mpgEntry} renderItem={renderRow} />
+      </SafeAreaView>
+    );
+  }
+
+
   SplashScreen.preventAutoHideAsync();
   setTimeout(SplashScreen.hideAsync, 2000);
 
@@ -22,7 +99,6 @@ export default function App()  {
   const [status, requestPermission] = MediaLibrary.usePermissions();
   const imageRef = useRef();
   const PlaceholderImage = require('./assets/vehicle.png')
-  const Drawer = createDrawerNavigator();
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -38,6 +114,57 @@ export default function App()  {
   };
 
   function HomeScreen({ navigation }) {
+    const [odoMiles, setOdoMiles] = useState(null);
+    const [milesSinceLast, setMilesSinceLast] = useState('');
+    const [mpg, setMpg] = useState('');
+    const [costOfFill, setCostOfFill] = useState('');
+    const [date, setDate] = useState('');
+    const [gallons, setGallons] = useState(null);
+    const [price, setPrice] = useState(null);
+
+    useEffect(() => {
+      db.transaction(tx => {
+        tx.executeSql(
+          'CREATE TABLE IF NOT EXISTS mpgEntries (id integer primary key not null, odoMiles real, milesSinceLast real, mpg real, costOfFill real, date real);'
+        );
+      });
+    }, []);
+
+    const saveEntry = () => {
+      const odoMiles = parseFloat(odoMiles);
+      const gallons = parseFloat(gallons);
+      const price = parseFloat(price);
+      const costOfFill = gallons * price;
+
+      if (odoMiles === null || gallons === null || price === null) {
+        Alert.alert('Please enter a valid number for mileage, gallons and price');
+        return;
+      }
+
+      const date = new Date().toLocaleDateString();
+      const costOfFillValue = gallons * price;
+      const milesSinceLast = odoMiles - (mpgEntry[0]?.odoMiles ?? odoMiles);
+      const mpgValue = milesSinceLast / gallons;
+      
+
+      db.transaction(
+        (tx) => {
+          tx.executeSql(
+            'INSERT INTO mpgEntries (odoMiles, milesSinceLast, mpg, costOfFill, date) VALUES (?, ?, ?, ?, ?)',
+            [odoMiles, milesSinceLast, mpgValue, costOfFillValue, date],
+            (txObj, resultSet) => console.log(resultSet.insertId),
+            (txObj, error) => console.log('Error', error)
+          );
+        },
+        null,
+        forceUpdate
+      );
+      setOdoMiles('');
+      setGallons('');
+      setPrice('');
+      navigation.navigate("Results", { mpgEntry: mpgEntry });
+    };
+
     return (
       <SafeAreaView style={styles.container}>
         <View style={styles.imageContainer}>
@@ -52,63 +179,44 @@ export default function App()  {
             style={styles.textInput} 
             placeholder="Mileage"
             keyboardType="decimal-pad"
+            returnKeyType='done'
+            defaultValue={odoMiles}
+            onChangeText={(odoMiles) => setOdoMiles(odoMiles)}
              />
           <Text style={styles.title}>Gallons of Gas</Text>
           <TextInput 
             style={styles.textInput} 
             placeholder="Gallons of Gas"
             keyboardType="decimal-pad"
+            returnKeyType='done'
+            defaultValue={gallons}
+            onChangeText={(gallons) => setGallons(gallons)}
             />
           <Text style={styles.title}>Price of Gas in Dollars Per Gallon</Text>
           <TextInput 
           style={styles.textInput} 
           placeholder="Price of Gas"
           keyboardType="decimal-pad"
+          returnKeyType='done'
+          defaultValue={price}
+          onChangeText={(price) => setPrice(price)}
           />
         </View>
         <View style={styles.buttonContainer}>
-          <Button theme="blue" label="Save" onPress={() => navigation.navigate("Results")} />
+          <Button theme="blue" label="Save" onPress={saveEntry} />
         </View>
       </SafeAreaView>
     );
   }
 
-  function ResultsScreen({ navigation }) {
-    function renderRow({ item }) {
-      return (
-        <View style={styles.row}>
-          <View style={styles.info}>
-            <Text style={styles.title}>Odometer miles: {item.odoMiles}</Text>
-            <Text style={styles.title}>Miles since last fill: {item.milesSinceLast}</Text>
-            <Text style={styles.title}>MPG: {item.mpg}</Text>
-            <Text style={styles.title}>Cost to fill: {item.costOfFill}</Text>
-            <Text style={styles.title}>Fillup date: {item.date}</Text>
-          </View>
-        </View>
-      );
-    };
-
-    data = [
-      {
-        odoMiles: 'example miles',
-        milesSinceLast: 'example miles',
-        mpg: 'example mpg',
-        costOfFill: 'example cost',
-        date: 'example date',
-      },
-      {
-        odoMiles: 'example miles 2',
-        milesSinceLast: 'example miles 2',
-        mpg: 'example mpg 2',
-        costOfFill: 'example cost 2',
-        date: 'example date 2',
-      },
-    ];
-
+  function ResultsScreen({ route }) {
+    console.log(route);
+    const mpgEntry = route.params?.mpgEntry ?? [];
+    console.log(mpgEntry);
     return (
       <View style={styles.container}>
         <Button theme="blue" label="Learn About MPG" onPress={() => WebBrowser.openBrowserAsync('https://learn.eartheasy.com/guides/how-to-calculate-gas-mileage/')} />
-        <FlatList data={data} renderItem={renderRow} />
+        <MpgEntries mpgEntry={mpgEntry} />
       </View>
     );
   }
@@ -117,10 +225,15 @@ export default function App()  {
     <NavigationContainer>
       <Drawer.Navigator useLegacyImplementation initialRouteName="Home">
         <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Results" component={ResultsScreen}  />
+        <Drawer.Screen name="Results" component={ResultsScreen} initialParams={{ mpgEntry: mpgEntry, setMpgEntry: setMpgEntry }} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
+}
+
+function useForceUpdate() {
+  const [value, setValue] = useState(0);
+  return [() => setValue(value + 1), value];
 }
 
 const styles = StyleSheet.create({
